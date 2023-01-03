94ec9899cdc3bafcee4e9b3b41036979e7787a0a
Markkam2003 <93402961+Markkam2003@users.noreply.github.com>
2023-01-03 14:47:38 -0800

    Initial commit

---
+++ giraffe/giraffe/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Giraffe Academy";
+            Console.WriteLine(phrase);
+
+            Console.ReadLine();
+        }
+    }
+}
